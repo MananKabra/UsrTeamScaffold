b254e78868625beaa9ee8c71c868e6c8484e24e3
harsh <harshpandey9324@gmail.com~>
2023-02-11 10:56:24 +0530

    Trial github upload

---
+++ lib/pages/loginpage.dart
@@ -0,0 +1,20 @@
+// Create a login page for the food app
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("Login Page"),
+          ],
+        ),
+      ),
+    );
+  }
+}
