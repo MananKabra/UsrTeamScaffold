479e89f4a3a4b969fe7b9b38c3a4ea6057b91fde
MananKabra <manankabra200318@gmail.com>
2023-02-11 11:03:39 +0530

    bc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unscriptrookiesfwm/starting_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Text("Hello"),
-      ),
+      home: StartingPage()
     );
   }
 }
